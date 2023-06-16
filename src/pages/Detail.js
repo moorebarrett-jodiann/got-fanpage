201e8f135972359de96524b8f67e238eebc67486
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2023-06-16 14:39:00 -0500

    Add Not Found

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Helmet } from 'react-helmet';
+import NotFound from './NotFound';
 
 
 function Detail() {
@@ -10,6 +11,7 @@ function Detail() {
     const { id } = useParams();
     const Character_API = `https://thronesapi.com/api/v2/Characters/${id}`;
     const [message, setMessage] = useState("Loading profile details ...");
+    const [notFound, setNotFound] = useState(false);
 
     useEffect(() => {
         axios
@@ -18,10 +20,15 @@ function Detail() {
                 setCharacterProfile(result.data);
             })
             .catch(error => {
+                setNotFound(true);
                 setMessage('Character details unavailable ...');
             });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id, Character_API]);
+
+    if(notFound){
+        return <NotFound />
+    }
 
     return (
         <>
